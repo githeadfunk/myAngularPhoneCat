f427f189e0ef53ee4fa6d9a5313e904531fa0268
githeadfunk <wyh388@gmail.com>
2017-05-29 17:15:40 -0500

    step-8

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,4 +1,4 @@
-var browserLogs = require('protractor-browser-logs');
+//var browserLogs = require('protractor-browser-logs');
 
 describe('PhoneCat Application', function() {
 
@@ -7,73 +7,62 @@ describe('PhoneCat Application', function() {
     var logs;  
     beforeEach(function() {
         browser.get('index.html');
-        logs = browserLogs(browser);
-        logs.ignore(logs.DEBUG);
-        logs.ignore(logs.INFO);
+//        logs = browserLogs(browser);
+//        logs.ignore(logs.DEBUG);
+//        logs.ignore(logs.INFO);
     });
       
-//    afterEach(function () {
-//        return logs.verify();
-//    });
-
-//    it('should filter the phone list as a user types into the search box', function() {
-//      var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
-//      var query = element(by.model('$ctrl.query'));
-//
-//      expect(phoneList.count()).toBe(3);
-//
-//      query.sendKeys('nexus');
-//      expect(phoneList.count()).toBe(1);
-//
-//      query.clear();
-//      query.sendKeys('motorola');
-//      expect(phoneList.count()).toBe(2);
-//    });
-      
-      it('should be able to ordered by name and age', function() {
-
-        var phoneList = element.all(by.repeater('phone in $ctrl.phones').column('phone.name'));
-        var order = element(by.model('$ctrl.order'));
+    it('should render phone specific links', function() {
         var query = element(by.model('$ctrl.query'));
-        
-        order.sendKeys('name');
-        console.log('123');
-        order.getAttribute('value').then(function(inputValue) {
-            console.log(inputValue);
-        });
-        
-        phoneList.getText().then(function(inputValue) {
-            console.log(inputValue);
-            expect(inputValue).toEqual([
-            'Nexus S',
-            'Motorola XOOM\u2122 with Wi-Fi',
-            'MOTOROLA XOOM\u2122'
-            ]);
-        });
-          
-        query.sendKeys('m');
-        phoneList.getText().then(function(inputValue) {
-            console.log(inputValue);
-            expect(inputValue).toEqual([
-            'Motorola XOOM\u2122 with Wi-Fi',
-            'MOTOROLA XOOM\u2122'
-            ]);
-        });
-//        expect(phoneList.count()).toBe(1);
-          
-//        browser.manage().logs().get('browser').then(function(browserLogs) {
-//           // browserLogs is an array of objects with level and message fields
-//           browserLogs.forEach(function(log){
-//              if (log.level.value > 900) { // it's an error log
-//                console.log('Browser console error!');
-////                console.log(query);
-//                console.log(log.message);
-//              }
-//           });
-//        });
+        query.sendKeys('nexus');
 
-
+        element.all(by.css('.phones li a')).first().click();
+        expect(browser.getLocationAbsUrl()).toBe('/phones/nexus-s');
     });
+//      it('should be able to ordered by name and age', function() {
+//
+//        var phoneList = element.all(by.repeater('phone in $ctrl.phones').column('phone.name'));
+//        var order = element(by.model('$ctrl.order'));
+//        var query = element(by.model('$ctrl.query'));
+//        
+//        order.sendKeys('name');
+//        console.log('123');
+//        order.getAttribute('value').then(function(inputValue) {
+//            console.log(inputValue);
+//        });
+//        
+//        phoneList.getText().then(function(inputValue) {
+//            console.log(inputValue);
+//            expect(inputValue).toEqual([
+//            'Nexus S',
+//            'Motorola XOOM\u2122 with Wi-Fi',
+//            'MOTOROLA XOOM\u2122'
+//            ]);
+//        });
+//          
+//        query.sendKeys('m');
+//        phoneList.getText().then(function(inputValue) {
+//            console.log(inputValue);
+//            expect(inputValue).toEqual([
+//            'Motorola XOOM\u2122 with Wi-Fi',
+//            'MOTOROLA XOOM\u2122'
+//            ]);
+//        });
+////        expect(phoneList.count()).toBe(1);
+//          
+////        browser.manage().logs().get('browser').then(function(browserLogs) {
+////           // browserLogs is an array of objects with level and message fields
+////           browserLogs.forEach(function(log){
+////              if (log.level.value > 900) { // it's an error log
+////                console.log('Browser console error!');
+//////                console.log(query);
+////                console.log(log.message);
+////              }
+////           });
+////        });
+
+
+//    });
 
   });
 
