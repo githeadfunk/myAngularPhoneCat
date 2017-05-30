a6715bd5ef37ed9e8bf55903aed62d02c687b253
githeadfunk <wyh388@gmail.com>
2017-05-29 21:48:44 -0500

    step-12

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -5,8 +5,14 @@ angular.
     controller: ['$http','$routeParams',
       function PhoneDetailController($http, $routeParams) {
         var self = this;
-        $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response){
-            self.phone = response.data;
+          
+        self.setImage = function setImage(imageUrl) {
+          self.mainImageUrl = imageUrl;
+        };
+
+        $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response) {
+          self.phone = response.data;
+          self.setImage(self.phone.images[0]);
         });
       }
     ]
